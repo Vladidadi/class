9573d48ea6e5aea9801f0715b490ec9a2fa27ba5
vvitmaer <vmvitmaer@protonmail.com>
2026-04-30 21:44:43 -0600

    final?

--- pj4/Project4_part2.c
+++ pj4/Project4_part2.c
@@ -1,89 +1,192 @@
-// Vladislav Vitamer
-#include <linux/kernel.h>
-#include <linux/sched/signal.h>
-#include <linux/module.h>
-#include <linux/mm.h>
+//Vladislav Vitamer
+   #include <linux/kernel.h>
+   #include <linux/sched.h>
+   #include <linux/module.h>
+   // #include <stdlib.h>
+   #include <linux/mm.h>
 
-static int pid_arg = 1;
-static char *va_string;
 
-module_param(pid_arg, int, 0);
-MODULE_PARM_DESC(pid_arg, "PID to inspect");
-module_param(va_string, charp, 0444);
-MODULE_PARM_DESC(va_string, "Virtual address to test (example: 0x7fff...)");
 
-static int find_in_range(struct task_struct *task)
+  void print_range(struct task_struct* ts);
+   int offset_fetcher(long addy, struct mm_struct *mm);
+   int find_in_range(struct task_struct* ts);
+  static int pid_arg =1;
+  static char* va_string;
+
+  struct vm_area_struct *vma;
+  unsigned long vm_space =0;
+  struct vma_iterator iter;
+
+  long start_bound=0xffffffffffff;
+  long end_bound=0;
+
+  module_param(pid_arg, int, 0);
+  MODULE_PARM_DESC(pid_arg, "The PID of the process whose information you'd like to see");
+  module_param(va_string, charp, 0444);
+  MODULE_PARM_DESC(   va_string, "The virtual address we want to check on");
+   
+   
+   int offset_fetcher(long addy, struct mm_struct *mm){
+      if((mm ))
 {
-	struct mm_struct *mm;
-	struct vm_area_struct *vma;
-	unsigned long query = 0;
-	int err;
-	int in_vma_range = 0;
 
-	if (!va_string) {
-		pr_info("va_string not set\n");
-		return 0;
-	}
+  long unsigned int vmpage;
+      int i=0;
+      pgd_t *pgd ;//=pgd_offset(mm,addy);
+      p4d_t *p4d;//=p4d_offset(pgd,addy);
+      pud_t *pud;//=pud_offset(p4d,addy);
+      pmd_t *pmd;//=pmd_offset(pud,addy);
+      pte_t *pte;//=pte_offset_kernel(pmd,addy);
+      // int found =0;
+      while(vma){
+         for (vmpage=vma->vm_start,i=1;vmpage < vma->vm_end;vmpage += PAGE_SIZE,i++){
+             pgd=pgd_offset(mm,addy);
+             printk(KERN_INFO "\n counter: %lx\n",vmpage);
+      if (!(pgd_none(*pgd) || pgd_bad(*pgd))){
+         // return 0;
+         p4d=p4d_offset(pgd,addy);
+         if (!(p4d_none(*p4d) || p4d_bad(*p4d))){
+            // return 0;
+            pud=pud_offset(p4d,addy);
+            if (!(pud_none(*pud) || pud_bad(*pud))){
+               // return 0;
+               pmd=pmd_offset(pud,addy);
+               if (!(pmd_none(*pmd) || pmd_bad(*pmd))){
 
-	err = kstrtoul(va_string, 0, &query);
-	if (err) {
-		pr_info("invalid va_string: %s (err=%d)\n", va_string, err);
-		return 0;
-	}
-
-	mm = get_task_mm(task);
-	if (!mm) {
-		pr_info("task %d has no userspace mm\n", task->pid);
-		return 0;
-	}
-
-	mmap_read_lock(mm);
-	vma = find_vma(mm, query);
-		pr_info("VMA: %lx-%lx\n", vma->vm_start,vma->vm_end);
-
-	if (vma && query >= vma->vm_start && query < vma->vm_end)
-		in_vma_range = 1;
-	mmap_read_unlock(mm);
-	mmput(mm);
-
-	pr_info("pid=%d va=%lx in_vma_range=%d\n", task->pid, query, in_vma_range);
-	return in_vma_range;
+                  pte=pte_offset_kernel(pmd,addy);
+                  return pte_present(*pte);
+                  
+               }
+            }
+         }
+      }
+      
+       
+         
+ 
+   }
+         }
+      }
+      return 0;
+     
 }
 
-int init_module(void)
-{
-	struct task_struct *iter;
-	int found = 0;
-	int query_in_range = 0;
 
-	pr_info("Entering VA VMA checker\n");
+  int find_in_range(struct task_struct* ts){
+     struct task_struct *task = ts;
+   printk(KERN_INFO "Entered loop in part3");
+   struct mm_struct *mm = get_task_mm(task);
+mmap_read_lock(mm);
 
-	for_each_process(iter) {
-		if (iter->pid == pid_arg) {
-			found = 1;
-			query_in_range = find_in_range(iter);
-			break;
-		}
-	}
+//    VMA_ITERATOR(iter,task->mm,0);
+//   for_each_vma(iter,vma){
+//    if(vma->vm_start < start_bound) {start_bound=vma->vm_start;}
+//    if(vma->vm_end > end_bound) {end_bound=vma->vm_end;}
+//    printk(KERN_INFO "heres a vma: start %lx      end %lx\n",   vma->vm_start,   vma->vm_end);
 
-	if (!found) {
-		pr_info("PID %d not found\n", pid_arg);
-		return 0;
-	}
+   
+//   }
+//   long range = end_bound-start_bound;
+//   printk(KERN_INFO "\nRange from %lx to %lx, with a total size of %lx",start_bound,end_bound,range);
 
-	if (query_in_range)
-		pr_info("The address is in the range of a VMA\n");
-	else
-		pr_info("The address is NOT in the range of a VMA\n");
+  long query =0;
+  bool err = kstrtol(va_string,0,&query);
+  if(err){
+   printk(KERN_INFO "\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!\nError in string to integer conversion\n\n");
+  }
 
-	return 0;
+
+  int present=offset_fetcher(query,mm);
+  mmap_read_unlock(mm);
+  printk(KERN_INFO "\n\nPresent status of page: %d\n",present);
+   return present;
 }
 
-void cleanup_module(void)
-{
-	pr_info("End VA VMA checker\n");
+  void print_range(struct task_struct* ts){
+     struct task_struct *task = ts;
+   printk(KERN_INFO "Entered loop in part3");
+   struct mm_struct *mm = get_task_mm(task);
+mmap_read_lock(mm);
+      //  char *print_state;
+   VMA_ITERATOR(iter,task->mm,0);
+  for_each_vma(iter,vma){
+   if(vma->vm_start < start_bound) {start_bound=vma->vm_start;}
+   if(vma->vm_end > end_bound) {end_bound=vma->vm_end;}
+   printk(KERN_INFO "heres a vma: start %lx      end %lx\n",   vma->vm_start,   vma->vm_end);
+
+   // print_range(iter);
+
+  }
+  mmap_read_unlock(mm);
+  long range = end_bound-start_bound;
+  printk(KERN_INFO "\nRange from %lx to %lx, with a total size of %lx",start_bound,end_bound,range);
+//   for(task=ts;task!=&init_task;task=task->parent)
+
+//    {
+//    switch(task->__state){
+//     case 0:
+//     print_state=    "TASK_RUNNING"   ;
+//     break;
+//     case 1:
+//     print_state= "TASK_INTERRUPTIBLE" ;
+//     break;
+//     default:
+//     print_state="error";
+//    }
+//    printk(KERN_INFO "heres a task named %s with PID [%d] whose state is %s\n",task->comm , task->pid,print_state);
+//    break;   
+// }
+
+   
 }
+
+  int init_module(void)
+  {
+
+        printk(KERN_INFO "Entering VA STATUS!\n");
+
+   struct task_struct *iter;
+   // short found = 0;
+bool query_in_range=0;
+   for_each_process(iter){
+      if(iter->pid==pid_arg){
+    printk(KERN_INFO "\n======================\n\nWE FOUND HIM\n\n======================\n");
+
+         
+         query_in_range = find_in_range(iter);
+    
+
+      }
+ 
+ 
+   }
+
+        if(query_in_range){
+      printk(KERN_INFO "\n\n The address is in the range of the vma\n\n");
+         
+      }else{
+      printk(KERN_INFO "\n\n The address is not in the range of the vma\n\n");
+      }
+
+
+
+   return 0;
+   }
+   
+   void cleanup_module(void)
+   {
+      //    VMA_ITERATOR(iter,task->mm,0);
+//   for_each_vma(iter,vma){
+//    if(vma->vm_start < start_bound) {start_bound=vma->vm_start;}
+//    if(vma->vm_end > end_bound) {end_bound=vma->vm_end;}
+//    printk(KERN_INFO "heres a vma: start %lx      end %lx\n",   vma->vm_start,   vma->vm_end);
+
+   
+   printk(KERN_INFO "End VA STATUS\n");
+   }
+
+
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Vladislav Vitmaer");
-MODULE_DESCRIPTION("Check whether virtual address belongs to a process VMA");
+MODULE_DESCRIPTION("Process information report");
